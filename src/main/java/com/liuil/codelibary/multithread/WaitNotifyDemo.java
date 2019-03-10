78dd99692dfc1f46ab6c3752b9b58ec66aa55aae
yang.liu <yang.liu@hypers.com>
2019-03-10 10:49:31 +0800

    feat: 增加synchronized和wait/notify示例代码

---
+++ src/main/java/com/liuil/codelibary/multithread/WaitNotifyDemo.java
@@ -0,0 +1,58 @@
+package com.liuil.codelibary.multithread;
+
+public class WaitNotifyDemo extends Thread {
+
+
+  public static void main(String[] args) {
+    Object lock = new Object();
+    new WaitThread(lock).start();
+    new NotifyThread(lock).start();
+    ;
+  }
+
+
+  static class WaitThread extends Thread {
+
+    private Object lock;
+
+    public WaitThread(Object lock) {
+      this.lock = lock;
+    }
+
+    @Override
+    public void run() {
+      synchronized (lock) {
+        System.out.println("Start wait Thread");
+        try {
+          lock.wait();
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+        System.out.println("Finish wait Thread");
+
+      }
+    }
+
+  }
+
+
+  static class NotifyThread extends Thread {
+
+    private Object lock;
+
+    public NotifyThread(Object lock) {
+      this.lock = lock;
+    }
+
+    @Override
+    public void run() {
+      synchronized (lock) {
+        System.out.println("Start notify thread");
+        lock.notify();
+        System.out.println("Finish notify thread");
+
+      }
+    }
+  }
+
+}
