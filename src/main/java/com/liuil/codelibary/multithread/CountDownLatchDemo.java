e816b3d9715a0fb812e83681d076bf94fd2726a8
yang.liu <yang.liu@hypers.com>
2019-03-16 11:03:40 +0800

    feat: 增加Condition, CountDownLatch, CyclicBarrier, Semaphore示例代码

---
+++ src/main/java/com/liuil/codelibary/multithread/CountDownLatchDemo.java
@@ -0,0 +1,29 @@
+package com.liuil.codelibary.multithread;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public class CountDownLatchDemo {
+
+  public static void main(String[] args) throws InterruptedException {
+    CountDownLatch countDownLatch = new CountDownLatch(3);
+    new Thread(() -> {
+      try {
+        TimeUnit.SECONDS.sleep(2);
+        countDownLatch.countDown();
+        System.out.println("After count minus to zero");
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }).start();
+
+    new Thread(() -> countDownLatch.countDown()).start();
+    new Thread(() -> countDownLatch.countDown()).start();
+
+    countDownLatch.await(5, TimeUnit.SECONDS);
+    System.out.println("After await finish");
+
+
+  }
+
+}
